94f29fee7bd4e2199a648b6b0b18f4f55b33faf1
strace <hamzasufyanmacrohard@gmail.com>
2026-07-08 13:47:50 +0500

    mingle: autocommit 2026-07-08 13:47:50

---
+++ bpe/bpe.v
@@ -0,0 +1,82 @@
+module main
+
+import os
+
+fn split_2(text string) []string {
+    mut z := ""
+    mut w := []string{}
+    for x in text.split("") {
+        for y in x {
+            if z.len < 2 {
+                z = "${z}${rune(y)}"
+            } else {
+                w << z
+                z = ""
+            }
+        }
+    }
+
+    return w
+}
+
+fn main() {
+    // byte pair encoding, oh yeah!
+    // res: https://en.wikipedia.org/wiki/Byte-pair_encoding
+    mut text := "The original BPE algorithm operates by iteratively replacing the most common contiguous sequences of characters in a target text with unused 'placeholder' bytes. The iteration ends when no sequences can be found, leaving the target text effectively compressed. Decompression can be performed by reversing this process, querying known placeholder terms against their corresponding denoted sequence, using a lookup table. In the original paper, this lookup table is encoded and stored alongside the compressed text."
+    text = text.to_lower()
+
+    // chinese failed: unused_placeholder_bytes := "人大大小小中上下天地心水火木山女子爱家学生月日明和国年时生友好新老高美文语信电电话车路书字口耳手足目食鱼鸟虫"
+    unused_placeholder_bytes := "ABCDEFGHIJKLMNOPQRSTUVQZYZ" // just use uppercase english
+
+    mut unused_placeholder_i := 0
+
+    mut byte_ranking := map[string]int
+
+    mut translation_table := map[string]string
+
+    // first, we need to divide text into: [][2]char - list of 2 characters.
+    bytes := split_2(text)
+
+    // now rank all byte pairs
+    for x in bytes {
+        byte_ranking[x] += 1
+    }
+
+    // test: verbose: println(byte_ranking)
+
+    // then we need unused place-holder bytes, how? lets use chinese or arabic ones? no, Lets use roman? I need atleast 50, let me check:
+    // we got unused_placeholder_bytes from web, easy!
+
+    for byte_pair, _ in byte_ranking {
+        if unused_placeholder_i < unused_placeholder_bytes.len {
+            bplaceholder := unused_placeholder_bytes[unused_placeholder_i]
+            unused_placeholder_i += 1
+            // test: verbose: println("replacing: ${byte_pair} -> ${rune(bplaceholder)}")
+            text = text.replace(byte_pair, rune(bplaceholder).str())
+            translation_table[byte_pair] = rune(bplaceholder).str()
+        } else {
+            break
+        }
+    }
+    // println(translation_table)
+    // println(text)
+
+    output_path := os.dir(os.executable())
+
+    translation_table_path := os.join_path(output_path,".","translation.table")
+    encoded_file_path := os.join_path(output_path,".","encoded.text")
+
+    os.write_file(translation_table_path, "${translation_table}") or {
+        println("writng to ${translation_table_path}: failed!")
+        return
+    }
+    println("writng to ${translation_table_path}: ok!")
+
+    os.write_file(encoded_file_path, text) or {
+        println("Writing to ${encoded_file_path}: failed!")
+        return
+    }
+    println("Writing to ${encoded_file_path}: ok!")
+    // done
+    return
+}
---
+++ mingle/mingle.v
@@ -0,0 +1,111 @@
+module main
+
+import os
+import time
+
+// Returns true on success, false on failure.
+fn add_all_and_commit(message string, repo string) bool {
+    // Step 1: git add .
+    add_args := ['git', '-C', repo, 'add', '.']
+    add_result := os.exec(add_args)
+
+    if add_result.exit_code != 0 {
+        return false
+    }
+
+    // Step 2: git commit -m "message"
+    commit_args := ['git', '-C', repo, 'commit', '-m', message]
+    commit_result := os.exec(commit_args)
+
+    return commit_result.exit_code == 0
+}
+
+
+fn main() {
+    // cd to ../
+    bin_path := os.executable()
+    dir_path := os.join_path(os.dir(bin_path), "..")
+    os.chdir(dir_path) or {
+        println('Error changing directory: ${err}')
+        return
+    }
+
+    // find all directories
+    entries := os.ls('.') or {
+        eprintln('Failed to read directory: ${err}')
+        return
+    }
+
+
+    mut dirs := []string{}
+    for entry in entries {
+        // Construct full path to check if it's a directory
+        full_path := os.join_path(dir_path, entry)
+
+        // Filter only directories (excluding symlinks to directories if desired)
+        if os.is_dir(full_path) {
+            dirs << full_path
+        }
+    }
+
+    // check if they contain mingle.ignore
+    mut dirs_not_to_commit := []string{}
+    for_dirs: for dir in dirs {
+        files := os.ls(dir) or {
+            eprintln('Failed to read directory: ${err}')
+            return
+        }
+
+        for_files: for file in files {
+            if os.is_file(os.join_path(dir, ".",file)) {
+                ig1 := file.split("/")
+                name := ig1[ig1.len - 1]
+                if name == "mingle.ignore" {
+                    dirs_not_to_commit << dir
+                    continue for_dirs
+                }
+            } else {
+                continue for_files
+            }
+        }
+    }
+    // if not then: `git add $dir && git commit -m "mingle: autocommit"`
+    for_dir: for dir in dirs {
+        if dir in dirs_not_to_commit {
+            println("ignored: ${dir}")
+            continue for_dir
+        }
+
+        /*
+        is_git := is_git_repo(dir)
+        println("comitting: ${dir} (is git repo? ${is_git})")
+        if !is_git {
+            println(" - initlized a git repo at: ${dir}")
+            init_git_repo(dir)
+        }
+
+        has_ignore_file := has_gitignore(dir)
+        if !has_ignore_file {
+            println(" - creating a .gitignore file at: ${dir}")
+            create_gitignore(dir)
+        }
+        */
+        direc1 := dir.split("/")
+        direc  := direc1[direc1.len-1]
+        side_proj := os.join_path(dir_path, ".", direc)
+        msg := "mingle: autocommit ${time.now()}"
+        println(" - commiting: ${side_proj}: message: ${msg}")
+        add_all_and_commit(msg, dir_path)
+    }
+    // now git push origin master
+    command := ["git","push","origin","master"]
+    cmd := os.exec(command)
+    println("executed: ${command} -- exit_code: ${cmd.exit_code}")
+    if cmd.exit_code != 0 {
+        println(" - err: ${cmd.output}")
+    } else {
+        println(" - success!")
+    }
+    // exit gracefully
+    return
+}
