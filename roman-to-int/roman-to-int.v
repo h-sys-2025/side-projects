862631f0812a6b01a421e793b4be090b7539c270
strace <hamzasufyanmacrohard@gmail.com>
2026-07-10 16:36:59 +0500

    mingle: autocommit 2026-07-10 16:36:59

--- roman-to-int/roman-to-int.v
+++ roman-to-int/roman-to-int.v
@@ -1,7 +1,21 @@
 module main
 
 fn main() {
-    roman_text := "XVII"
+    mut roman_text := map[string]int
+    roman_text["X"] = 10
+    roman_text["XVII"] = 17
+    roman_text["XII"] = 12
+    roman_text["XIV"] = 14
+    roman_text["IV"] = 4
+
+    for k,v in roman_text {
+        q := roman_to_int_voila(k)
+        if q == v {
+            println("passed: ${k} == ${q}")
+        } else {
+            println("failed: ${k} != ${q} (was supposted to be ${v})")
+        }
+    }
     /* thinking:
      *
      * 1. XVII (becomes) IIVX
@@ -13,22 +27,38 @@ fn main() {
      * 4. V (add 2 to 5, sum is 7)
      *
      * 5. X (add 10 to 8, sun is 17)
+     *
+     * 6. if last > current, then -current!
      */
-
-     println("roman: ${roman_text} to int is ${roman_to_int_voila(roman_text)}")
 
     return
 }
 
 fn roman_to_int_voila(roman_text string) int {
     mut summmm := 0
+    mut last := 0
     for whatever in roman_text.to_lower().split("").reverse() {
         if whatever == "i" {
-            summmm += 1
+            if last > 1 {
+                summmm -= 1
+            } else {
+                summmm += 1
+            }
+            last = 1
         } else if whatever == "v" {
-            summmm += 5
+            if last > 5 {
+                summmm -= 5
+            } else {
+                summmm += 5
+            }
+            last = 5
         } else if whatever == "x" {
-            summmm += 10
+            if last > 10 {
+                summmm -= 10
+            } else {
+                summmm += 10
+            }
+            last = 10
         } else {
             eprintln("NUMERAL ${whatever} is not supported yeat!")
             return 0
