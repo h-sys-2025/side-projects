01ccc9ece5466e832a5c6fdb51ac705868eb763b
strace <hamzasufyanmacrohard@gmail.com>
2026-07-13 19:06:46 +0500

    mingle-v2: 2026-07-13 19:06:46

--- generics/v_generics.v
+++ generics/v_generics.v
@@ -12,9 +12,12 @@ fn compare[T](a T, b T) int {
 }
 
 //@ generics in v are awsome!
-//@ but on the go, i found about the harsh reality!
-//@ v generates 4k lines of boilerplate!!!!
-//@ just try DCE brah!
+// but on the go, i found about the harsh reality!
+// v generates 4k lines of boilerplate!!!!
+// just try DCE brah!
+//@ for more info on generics, take a look: ./higher-order-functions/*here*
+//@
+//@ someday, I will try to make REAL generics in C, someday?!.
 fn main() {
     println(compare(2,55))
     return
