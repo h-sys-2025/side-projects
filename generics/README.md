b893d646fd734ebdd226fa719c082dabf7f38710
strace <hamzasufyanmacrohard@gmail.com>
2026-07-15 14:28:19 +0500

    mingle-v2: 2026-07-15 14:28:19

---
+++ generics/README.md
@@ -0,0 +1,8 @@
+# Today I woke up and choose violence!
+
+# Implement generics in pure c99.
+- Just for fun. (recreational-programming)
+
+## Rules:
+1. dont use macros.
+2. dont make it repetitive.
--- generics/c_generics.c
+++ generics/c_generics.c
@@ -1,88 +1,101 @@
-/*
-
-    Today I woke up and choose violence!
-
-    # Implement generics in pure c99.
-    - Just for fun. (recreational-programming)
-    ## Rules:
-    1. dont use macros.
-    2. dont make it repetitive.
-
-*/
 #include <stdio.h>
 #include <stdint.h>
 
-typedef enum { i64, i32, f64, f32, string } typetype;
+typedef enum { I64, I32, F64, F32, STRING } Type;
 
 typedef struct {
     void* data;
-    typetype type;
-} any;
+    Type  type;
+} Any;
+
+#define _type(t) _type_##t
+
+#define _type_I64   int64_t
+#define _type_I32   int32_t
+#define _type_F64   double
+#define _type_F32   float
+#define _type_STRING const char*
 
 #define var(TYP, VAL) ({ \
     static __typeof__(VAL) _val_##__LINE__ = (VAL); \
-    (any){ .data = &_val_##__LINE__, .type = TYP }; \
+    (Any){ .data = &_val_##__LINE__, .type = TYP }; \
 })
 
-const char* typen(typetype t) {
-    switch (t) {
-        case i64: return "i64";
-        case i32: return "i32";
-        case f64: return "f64";
-        case f32: return "f32";
-        case string: return "char[]";
-        default:  return "unknown";
+#define cast(A, TYPE) (*(TYPE*)((A).data))
+
+#define cast_any(A) _cast_any((A))
+static inline double _cast_any(Any a) {   // returns double for simplicity, you can extend
+    switch (a.type) {
+        case I64: return (double)*(int64_t*)a.data;
+        case I32: return (double)*(int32_t*)a.data;
+        case F64: return *(double*)a.data;
+        case F32: return (double)*(float*)a.data;
+        default:  return 0.0;
     }
 }
 
-// A simple macro to cast `void*` to the required type, using enum.
-// returns the value (not a pointer) yayyy!
-#define cast_any(A) ({ \
-    __typeof__(0) _res; \
-    any _tmp = (A); \
+const char* type_name(Type t) {
+    switch (t) {
+        case I64:    return "i64";
+        case I32:    return "i32";
+        case F64:    return "f64";
+        case F32:    return "f32";
+        case STRING: return "char[]";
+        default:     return "unknown";
+    }
+}
+
+#define print_any(A) do { \
+    Any _tmp = (A); \
     switch (_tmp.type) { \
-        case i64:  _res = *(int64_t*)_tmp.data; break; \
-        case i32:  _res = *(int32_t*)_tmp.data; break; \
-        case f64:  _res = *(double*)_tmp.data; break; \
-        case f32:  _res = *(float*)_tmp.data; break; \
-        default:   _res = 0; break; \
+        case I64: \
+            printf("Type: %s, Value: %ld\n", type_name(_tmp.type), *(int64_t*)_tmp.data); \
+            break; \
+        case I32: \
+            printf("Type: %s, Value: %d\n", type_name(_tmp.type), *(int32_t*)_tmp.data); \
+            break; \
+        case F64: \
+            printf("Type: %s, Value: %f\n", type_name(_tmp.type), *(double*)_tmp.data); \
+            break; \
+        case F32: \
+            printf("Type: %s, Value: %f\n", type_name(_tmp.type), *(float*)_tmp.data); \
+            break; \
+        case STRING: \
+            printf("Type: %s, Value: %s\n", type_name(_tmp.type), (char*)_tmp.data); \
+            break; \
+        default: \
+            printf("Type: unknown\n"); \
+            break; \
     } \
-    _res; \
-})
+} while(0)
 
-// since a macro cannot change its return type (it's either int or a double or whatever.),
-// you usually cast to a specific type you want, or use a helper block (maybe).
-// if you want to print it directly, you can use the print_any() macro!
+Any add(Any a, Any b) {
+    // For demo: promote everything to double
+    double x = cast_any(a);
+    double y = cast_any(b);
+    static double result;
+    result = x + y;
+    return (Any){ .data = &result, .type = F64 };
+}
 
-// generic printer macro
-#define print_any(A) ({ \
-    any _tmp = (A); \
-    switch (_tmp.type) { \
-        case i64:  printf("Type: %s, Value: %ld\n", typen(_tmp.type), *(int64_t*)_tmp.data); break; \
-        case i32:  printf("Type: %s, Value: %d\n", typen(_tmp.type), *(int32_t*)_tmp.data); break; \
-        case f64:  printf("Type: %s, Value: %f\n", typen(_tmp.type), *(double*)_tmp.data); break; \
-        case f32:  printf("Type: %s, Value: %f\n", typen(_tmp.type), *(float*)_tmp.data); break; \
-        default:   printf("Type: unknown\n"); break; \
-    } \
-})
+int main(void) {
+    Any pi   = var(F64, 22.0/7.0);
+    Any count = var(I32, 42);
+    Any big  = var(I64, 9000000000LL);
+    Any name = var(STRING, "Violence");
 
-#define cast_any_as(A, TYPE) (*(TYPE*)((A).data))
+    // print_any(pi);
+    // print_any(count);
+    // print_any(big);
+    // print_any(name);
 
-int main() {
+    _type(F64) d_val = cast(pi, _type(F64));
+    _type(I32) i_val = cast(count, _type(I32));
 
-    any pi = var(f64, 22.0/7.0);
-    any count = var(i32, 42);
-    any big = var(i64, 9000000000LL);
+    printf("\nCalculation: %f + 10 = %f\n", d_val, d_val + 10.0);
 
-    print_any(pi);
-    print_any(count);
-    print_any(big);
-
-    // get the value for math, we must say the type we want!
-    double d_val = cast_any(pi);
-    int i_val = cast_any_as(count, int);
-
-    printf("Calculation: %f + 10 = %f\n", d_val, d_val + 10.0);
+    Any sum = add(pi, count);
+    print_any(sum);
 
     return 0;
 }
