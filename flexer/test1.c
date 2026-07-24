48eaa4a100a75b61ae831c144324a6fa5aa9415f
strace <hamzasufyanmacrohard@gmail.com>
2026-07-24 18:21:06 +0500

    vingle: autocommit: 2026-07-24 13:21:06.546340Z

--- flexer/test1.c
+++ flexer/test1.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /* # For tgc.v */
-fn add<T>(T a, T b) {
+fn add(T a, T b) {
   return a + b;
 }
 
 int main(void) {
-  double res = add<double>(22.0/7.0, 100.0/3.0);
+  double res = add(22.0/7.0, 100.0/3.0);
   printf("res is %f", res);
   return 123;
 }
