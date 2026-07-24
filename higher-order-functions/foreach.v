04b4d4198d5cdac7a8bec68393bff4ca4c736f1a
strace <hamzasufyanmacrohard@gmail.com>
2026-07-24 18:40:41 +0500

    vingle: autocommit: 2026-07-24 13:40:41.558828Z

--- higher-order-functions/foreach.v
+++ higher-order-functions/foreach.v
@@ -43,6 +43,17 @@ pub fn (mut llist List[T]) map[U](func fn(T) U) List[U] {
     return list_u
 }
 
+pub fn (mut llist List[T]) filter(func fn(T, List[T]) bool) {
+    mut list_filtered := List[T]{}
+    for x in 0..llist.items.len {
+        if func(llist.items[x], llist) == true {
+          list_filtered.items << llist.items[x]
+        }
+    }
+
+    llist.items = list_filtered.items
+}
+
 pub fn (mut llist List[T]) foreach(func fn(T) ?T) {
     for x in 0..llist.items.len {
         item_before := llist.items[x]
@@ -53,34 +64,65 @@ pub fn (mut llist List[T]) foreach(func fn(T) ?T) {
     }
 }
 
-// main function (testing mingle again, ignore this!!)
+//@ main function (testing mingle again, ignore this!!)
 fn main() {
-    // generics are awsome
-    mut names := List[string]{}
+  // generics are awsome
+  mut names := List[string]{}
 
-    names.push("hamza")
-    names.push("someone")
-    names.push("noone")
+  names.push("hamza")
+  names.push("someone")
+  names.push("noone")
 
-    println("pop: ${names.pop()}")
-    println("len: ${names.len}")
-    println("contains \"hamza\": ${names.contains("hamza")}")
-    println("contains \"qwerty\": ${names.contains("qwerty")}")
+  println("pop: ${names.pop()}")
+  println("len: ${names.len}")
+  println("contains \"hamza\": ${names.contains("hamza")}")
+  println("contains \"qwerty\": ${names.contains("qwerty")}")
 
-    // the big thing!
-    // higher-order-functions!
-    names.foreach(fn(x string) ?string {
-        return "${x}1111"
-    })
+  // the big thing!
+  // higher-order-functions!
+  names.foreach(fn(x string) ?string {
+      return "${x}1111"
+  })
 
-    // now lets try to convert!
-    mut nnames := names.map[[]string](fn(x string) []string {
-        return x.split("")
-    })
+  // now lets try to convert!
+  mut nnames := names.map[[]string](fn(x string) []string {
+      return x.split("")
+  })
 
-    nnames.foreach(fn(x []string) ?[]string {
-        println("converted to []string: ${x} it was originally \"${x.join("")}\"")
-        return none
-    })
-    return
+  nnames.foreach(fn(x []string) ?[]string {
+      println("converted to []string: ${x} it was originally \"${x.join("")}\"")
+      return none
+  })
+
+  mut primes := List[u64]{}
+  for x in 1..250 {
+    primes.items << x
+  }
+
+  primes.filter(fn(item u64, primes List[u64]) bool {
+    if item < 2 {
+      return false
+    }
+
+    if item == 2 {
+      return true
+    }
+    if item % 2 == 0 {
+      return false
+    }
+
+    for x in primes.items {
+      if x * x > item {
+          panic("overflow!")
+      }
+
+      if item % x == 0 {
+          return false
+      }
+    }
+
+    return true // No divisors found after checking all necessary primes
+  })
+  println("prime numbers are: ${primes}")
+  return
 }
