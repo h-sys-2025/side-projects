90f4d4549f3b2db51fd3e076ede391d3bb9c03cd
strace <hamzasufyanmacrohard@gmail.com>
2026-07-27 18:38:45 +0500

    vingle: autocommit: 2026-07-27 13:38:45.706183Z

---
+++ markov-elixir/tokenizer.exs
@@ -0,0 +1,70 @@
+defmodule Tokenizer do
+  # this will tokenize it!
+  # first, it will have to .to_lower() all of it!
+  # then,  split by special-chars and spaces
+  # then,  return all items.
+  def tokenize(data) do
+    {data} =
+      case data do
+        "" ->
+          IO.puts("data is empty.")
+          System.halt()
+        _ ->
+          {data}
+      end
+
+    u1 = String.split(data, "\n", trim: true)
+    {no_lines} =
+      case Enum.count(u1) > 0 do
+        true ->
+          {u1}
+        false ->
+          IO.puts("there are no lines in data file. (tokenizer.exs)")
+          System.halt()
+      end
+
+    delim_lines =
+      no_lines
+      |> Enum.join("|")
+      |> String.split(" ")
+      |> Enum.join("|")
+
+    special_chars = "!@#$%^&*(),.?:;"
+
+    # --- start AI code ---
+    pattern = Regex.escape(special_chars) |> then(&"[#{&1}]")
+    regex = ~r/(#{pattern})/
+    no_special_chars = Regex.replace(regex, delim_lines, "|\\1|")
+    # --- end AI code ---
+
+    no_special_chars =
+      no_special_chars
+      |> String.split("||")
+      |> Enum.join("|")
+
+    no_special_chars
+  end
+end
+
+defmodule Main do
+  def main() do
+    data_file = "./training_corpus.data"
+
+    {data} =
+      case File.read(data_file) do
+        {:ok, content} ->
+          {content}
+        {_, _} ->
+          IO.puts("failed to read data file: #{data_file}")
+          System.halt()
+          {""}
+      end
+
+    tokens = Tokenizer.tokenize(data)
+    IO.puts("tokens: #{tokens}")
+  end
+end
+
+Main.main()
+
+# will be continued here:
