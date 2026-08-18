3953f8999ccd21971adfc134553fce30b31cf2d7
strace <hamzasufyanmacrohard@gmail.com>
2026-08-18 11:55:07 +0500

    vingle: autocommit: 2026-08-18 06:55:07.071224Z

---
+++ c-dynamic-codegen/main.v
@@ -0,0 +1,20 @@
+module main
+
+import ccg {c_program, ret, varval, vardef, func_call}
+
+fn main() {
+
+  mut hello_world := c_program()
+
+  // main function.
+  hello_world.new_func("main", "int", ["int argc", "char** argv"])
+  hello_world.func_add_stmt("main",
+    vardef("string", "message", "Hello, Sailor!")
+  )
+  hello_world.func_add_stmt("main",
+    func_call("printf",[varval("message")])
+  )
+  hello_world.func_add_stmt("main", ret("0"))
+  hello_world.print_func("main")
+  return
+}
