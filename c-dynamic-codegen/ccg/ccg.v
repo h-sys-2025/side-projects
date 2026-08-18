6639fbb1ae763e0e21c35b0b0f8dbc6bdd62c5c9
strace <hamzasufyanmacrohard@gmail.com>
2026-08-18 11:57:39 +0500

    vingle: autocommit: 2026-08-18 06:57:39.901276Z

--- c-dynamic-codegen/ccg/ccg.v
+++ c-dynamic-codegen/ccg/ccg.v
@@ -1,5 +1,6 @@
 module ccg
 
+//@ all functions will have this sig
 struct Func {
   pub mut:
     name string
@@ -8,6 +9,7 @@ struct Func {
     body []string
 }
 
+//@ all structs, wither typedef or not will have this sig
 struct Sstruct {
   pub mut:
     name string      // Sstruct
@@ -15,16 +17,19 @@ struct Sstruct {
     items []string   // ["string name;", "bool is_typedef;", "string[] items;"]
 }
 
+//@ our dynamic-ly generated c progam
 pub struct C_dyn_ccg {
   pub mut:
     functions map[string]Func
     structs   map[string]Sstruct
 }
 
+//@ initlizer for C_dyn_ccg
 pub fn c_program() C_dyn_ccg {
   return C_dyn_ccg{}
 }
 
+//@ create new function
 pub fn (mut c_cg C_dyn_ccg) new_func(name string, ret_type string, args []string) {
   c_cg.functions[name] = Func{
     name: name
@@ -33,26 +38,32 @@ pub fn (mut c_cg C_dyn_ccg) new_func(name string, ret_type string, args []string
   }
 }
 
+//@ add an stmt into an existign function
 pub fn (mut c_cg C_dyn_ccg) func_add_stmt(name string, stmt string) {
   c_cg.functions[name].body << stmt
 }
 
+//@ declare a variable
 pub fn vardef(ret_type string, name string, value string) string {
   return "${ret_type} ${name} = ${value};"
 }
 
+//@ call the value of a val
 pub fn varval(name string) string {
   return "${name}"
 }
 
+//@ write return statement
 pub fn ret(thing string) string {
   return "return ${thing};"
 }
 
+//@ call a function
 pub fn func_call(name string, args []string) string {
   return "${name}(${args.join(",")});"
 }
 
+//@ print the function in console
 pub fn (mut c_cg C_dyn_ccg) print_func(name string) {
   mut func := c_cg.functions[name]
   println("${func.ret_type} ${func.name}(${func.args.join(", ")}) {\n  ${func.body.join("\n  ")}\n}")}
