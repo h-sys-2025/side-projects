f676eeb824d75a6b0cd9076931d58dce2b562a34
strace <hamzasufyanmacrohard@gmail.com>
2026-08-20 11:47:39 +0500

    vingle: autocommit: 2026-08-20 06:47:39.545273Z

--- slice-in-c/slice.c
+++ slice-in-c/slice.c
@@ -3,45 +3,42 @@
 #include <string.h>
 
 /*
-  @desc: simple abstraction for slice statements in C language. only works for `char*` for now.
-  @usage: auto slice(char*, size_t, size_t) -> char* {...}
+  # slice:
+  - simple abstraction for slice things in `C` language. only supports `char*` for now.
+
+  // syntax to use in `koilaC` (my implementation of  C).
+  include "slice.so" {
+    auto slice(char*, size_t, size_t) -> char* {...}
+  }
 */
 char *slice(char *sentence, size_t i, size_t j) {
-  if (j > strlen(sentence)) {
-    // memory safety.
-    return NULL;
-  }
+  // safe-guards.
+  if (sentence == NULL || i >= j) return NULL; // pointer trouble, null pointer derefrence and parameter polluting.
 
-  if (i < 0) i = 0;
+  size_t str_len = strlen(sentence);
 
-  // the lenght of resultant slice.
+  if (i >= str_len) return NULL; // You cannot start slicing where pointer data has already been ended.
+  if (j > str_len) j = str_len;  // You cannot get more then you deserve.
+
+  // length of resaltant slice data.
   size_t len = j - i;
 
-  // memory allocation for slice.
-  char *res = malloc(
-    (len + 1) * sizeof(char)
-  );
-  if (res == NULL) {
-    return res;
-  }
+  // critical memory allocation for slice, only `C` folkes might understand.
+  char *res = malloc((len + 1) * sizeof(char));
+  if (res == NULL) return res; // looking for NULL pointers.
 
-  size_t u = 0;
-  while (u < len) {
-    res[u] = sentence[i+u];
-    u++;
-  }
-  res[u] = '\0'; // a trailing null byte.
+  memcpy(res, &sentence[i], len); /* mov res, [sentence+len] */
+  res[len] = '\0'; // also put a trailing null byte.
 
   return res;
 }
 
 int main() {
-  char *sentence = "The quick brown fox jumps off a lazy dog.";
-  char *sliced   = slice(sentence, 11, 29);
+  char *sentence = "The quick brown fox jumps higher then the lazy dog.";
+  char *sliced = slice(sentence, 11, 69);
 
-  if (sliced != NULL) {
-    printf("%s\n", sliced); // Outputs: "ck brown fox "
-
+  if (sliced != NULL) { // pointer trouble.
+    printf("%s\n", sliced);
     free(sliced);
   }
 
