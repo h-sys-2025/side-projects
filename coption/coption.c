31e9df10b57b17fdd9756418b78655be097da364
strace <hamzasufyanmacrohard@gmail.com>
2026-08-28 11:28:04 +0500

    vingle: autocommit: 2026-08-28 06:28:04.250623Z

--- coption/coption.c
+++ coption/coption.c
@@ -1,32 +1,39 @@
 #include <stdio.h>
-#include <strings.h>
+#include <string.h>
 #include <stdbool.h>
 
-#define Option_define(X) typedef struct { \
-  X Some; \
-  bool None; \
-} Option_##X;
+#define concat_lvl_2_macro_expansion_because_macros_are_not_that_powerfull_and_safe_to_use(A, B) A##B
+#define concat(A, B) concat_lvl_2_macro_expansion_because_macros_are_not_that_powerfull_and_safe_to_use(A, B)
 
-// -- define option of boolean.
+#define Option_define(s) typedef struct { \
+  s Some; \
+  bool None; \
+} concat(Option_, s);
+
+// -- define option of bool type.
 Option_define(bool)
 
-#define Option(X) (Option_##X)
-#define Some(X,Y) (Option_##X){.Some = (Y), .None = false}
-#define None(X) (Option_##X){.None = true}
-#define isSome(X) ((X).None == false)
-#define getSome(X) ((X).Some)
+#define Option(s) concat(Option_, s)
+#define Some(s,Y) (Option(s)){.Some = (Y), .None = false}
+#define None(s,Y) (Option(s)){.None = true}
+#define isSome(s) (s.None == false)
+#define getSome(s) (s.Some)
+
+// --- usage ---
+
 Option(bool) isAdmin(const char* passwd) {
-  if (strcmp(passwd, "demo#passwd@1234")) {
-    return None(bool);
-  } else if (strcmp(passwd, "outgrabe")) {
-    return Some(bool, true);
-  }
-  return Some(true);
+    if (strcmp(passwd, "demo#passwd@1234") == 0) {
+        return None(bool, false);
+    } else if (strcmp(passwd, "outgrabe") == 0) {
+        return Some(bool, true);
+    }
+    return Some(bool, false);
 }
 
 int main() {
-  Option(bool) x = isAdmin("outgrabe");
-  if (isSome(x))
-    printf("%s",getSome(x));
-  return 0;
+    Option(bool) x = isAdmin("outgrabe");
+    if (isSome(x)) {
+        printf("%d\n", getSome(x));
+    }
+    return 0;
 }
